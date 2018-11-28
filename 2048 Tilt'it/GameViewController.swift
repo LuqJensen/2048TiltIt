50e39b203945975a386dedf69effcc95786a8a50
Thanusaan Rasiah <rasiahmt@gmail.com>
2018-11-28 15:35:55 +0100

    Tilt to move implementation

--- 2048 Tilt'it/GameViewController.swift
+++ 2048 Tilt'it/GameViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import CoreMotion
 
 class GameViewController: UIViewController {
+    
+    var motionManager = CMMotionManager()
     
     var game = Game()
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        gameView.generateTiles(game.matrix)
+        gameView.setNeedsDisplay()
+        gameView.setNeedsLayout()
+        
+        motionManager.deviceMotionUpdateInterval = 0.2
+        
+        var lastMotion = ""
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                 if myData.attitude.pitch <= 0.4 {
+                    if lastMotion != "up" {
+                        self.game.moveUp()
+                    }
+                    lastMotion = "up"
+                }
+                else if myData.attitude.pitch > 1.4 {
+                    if lastMotion != "down" {
+                        self.game.moveDown()
+                    }
+                    lastMotion = "down"
+                }
+                else if myData.attitude.roll <= -1 {
+                    if lastMotion != "left" {
+                        self.game.moveLeft()
+                    }
+                    lastMotion = "left"
+                }
+                else if myData.attitude.roll > 1 {
+                    if lastMotion != "right" {
+                        self.game.moveRight()
+                    }
+                    lastMotion = "right"
+                }
+                 else {
+                    lastMotion = ""
+                }
+                self.gameView.setTiles(newValues: self.game.matrix)
+                self.scoreLabel.text = "Score: \(self.game.score)"
+            }
+        }
     }
 
     @IBOutlet weak var gameView: GameView!
@@ -22,23 +68,8 @@ class GameViewController: UIViewController {
     @IBAction func move(_ sender: UIButton) {
         let direction = sender.currentTitle
         switch direction {
-        case "Left":
-            game.moveLeft()
-        case "Right":
-            game.moveRight()
-        case "Up":
-            game.moveUp()
-        case "Down":
-            game.moveDown()
-        case "Start":
-            gameView.generateTiles(game.matrix)
-            gameView.setNeedsDisplay()
-            gameView.setNeedsLayout()
-        default: game.moveLeft()
+        default: print()
         }
-        gameView.setTiles(newValues: game.matrix)
-        
-        scoreLabel.text = "Score: \(game.score)"
     }
 }
 
